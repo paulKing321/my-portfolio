0b8210b4c1148716d7ea65278a0740937b85f487
paulking321 <oluwadamilare321@gmail.com>
2021-11-03 10:05:29 +0100

    .

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,7 +10,7 @@
       <img class="w-60 mx-40 border-4 border-purple-700 rounded-md " src="../assets/paulking.jpg">
     </div>
     <router-link to="/projects"
-        class="text-white px-4 py-2 m-1 rounded"
+        class="text-white px-4 py-2 m-1 bg-purple-700 border-4 border-purple-700 rounded-full"
         exact-active-class="bg-purple-700"
         >My Projects</router-link>
   </div>
