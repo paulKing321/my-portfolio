27d859cdab5e4400812406c3901f10cb9a990fd0
paulking321 <oluwadamilare321@gmail.com>
2021-11-10 13:31:33 +0100

    contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,19 @@
 <template>
-  <div></div>
+  <div>
+
+  </div>
 </template>
 
 <script>
 export default {
-
+data() {
+  return {
+    phone: +2349030244034,
+    email: oluwadamilare321@gmail.com,
+    facebook: https://facebook.com/adeoye paul oluwadamilare,
+    github: https://github.com/paulKing321,
+  }
+}
 }
 </script>
 
